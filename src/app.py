5b65b5d44fe067853c71957b1e65013995544955
hydroptix <samuelfrazee@hotmail.com>
2020-04-17 13:19:13 -0700

    Part 5 - POST operations

--- src/app.py
+++ src/app.py
@@ -1,24 +1,43 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hi Minh'
+    return 'Hello world'
 
 
-@app.route('/users/<id>')
-def get_users(id):
-    if id:
-        for user in users['users_list']:
-            if user['id'] == id:
-                return user
+# @app.route('/users/<id>')
+# def get_users(id):
+#     if id:
+#         for user in users['users_list']:
+#             if user['id'] == id:
+#                 return user
+#
+#         return ({})
+#
+#     return users
 
-        return ({})
 
-    return users
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        if search_username:
+            subdict = {'users_list': []}
+            for user in users['users_list']:
+                if user['name'] == search_username:
+                    subdict['users_list'].append(user)
+            return subdict
+        return users
 
+    elif request.method == 'POST' :
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success=True)
+
+        return resp
 
 users = {
     'users_list':
